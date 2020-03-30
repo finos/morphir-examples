c9f689a6941cb21db555e86c9013d4a7e941867f
reesh-a <Reeshabh.Agrawal@morganstanley.com>
2020-03-30 18:49:16 -0400

    Conforming BooksAndRecords to new stateful app type

--- src/Morphir/Sample/Apps/BooksAndRecords/App.elm
+++ src/Morphir/Sample/Apps/BooksAndRecords/App.elm
@@ -1,95 +1,80 @@
-module Morphir.Sample.Apps.BooksAndRecords.App exposing (..)
+module Morphir.Dapr.Input.Example exposing (..)
+
+import Morphir.SDK.StatefulApp exposing (StatefulApp)
 
 
-import Dict exposing (Dict)
-import Morphir.SDK.App exposing (StatefulApp, statefulApp, cmdNone)
-import Morphir.Sample.Apps.Shared.Product as Product
-import Morphir.Sample.Apps.Shared.Price exposing (..)
-import Morphir.Sample.Apps.BooksAndRecords.Deal exposing (..)
+type alias ID =
+    String
 
 
-type alias App = 
-    StatefulApp API RemoteState LocalState Event
+type alias ProductID =
+    String
 
 
-type alias API =
-    { openDeal : ID -> Product.ID -> Price -> Quantity -> Result OpenRequestFault Event
-    , closeDeal : ID -> Result CloseRequestFault Event
+type alias Price =
+    Float
+
+
+type alias Quantity =
+    Int
+
+
+type alias Deal =
+    { id : ID
+    , product : ProductID
+    , price : Price
+    , quantity : Quantity
     }
 
 
-type alias RemoteState =
-    ()
+type DealCmd
+    = OpenDeal ID ProductID Price Quantity
+    | CloseDeal ID
 
 
-type alias LocalState = 
-    Dict ID Deal
-
-
-type Event
-    = DealOpened ID Product.ID Price Quantity
+type DealEvent
+    = DealOpened ID ProductID Price Quantity
     | DealClosed ID
+    | InvalidQuantity ID Quantity
+    | InvalidPrice ID Price
+    | DuplicateDeal ID
+    | DealNotFound ID
+
+
+type alias App =
+    StatefulApp ID DealCmd Deal DealEvent
 
 
 app : App
 app =
-    statefulApp
-        { api = api
-        , init = init
-        , update = update
-        , subscriptions = subscriptions
-        }
+    StatefulApp logic
 
 
-api : RemoteState -> LocalState -> API
-api remote local =
-    { openDeal = \dealID productID price quantity ->
-        if price < 0 then
-            Err (InvalidPrice dealID price)
-        else if price < 0 then
-            Err (InvalidQuantity dealID quantity)
-        else if Dict.member dealID local then
-            Err (DuplicateDeal dealID)
-        else
-            Ok (DealOpened dealID productID price quantity)
+logic : ID -> Maybe Deal -> DealCmd -> ( String, Maybe Deal, DealEvent )
+logic dealId deal dealCmd =
+    case deal of
+        Just d ->
+            case dealCmd of
+                CloseDeal _ ->
+                    ( dealId, Nothing, DealClosed dealId )
 
-    , closeDeal = \dealID ->
-        case local |> Dict.get dealID of
-            Nothing ->
-                Err (DealNotFound dealID)
+                OpenDeal _ _ _ _ ->
+                    ( dealId, deal, DuplicateDeal dealId )
 
-            Just _ ->
-                Ok (DealClosed dealID)
-    }
+        Nothing ->
+            case dealCmd of
+                OpenDeal id productId price qty ->
+                    if price < 0 then
+                        ( dealId, deal, InvalidPrice id price )
 
+                    else if qty < 0 then
+                        ( dealId, deal, InvalidQuantity id qty )
 
-init : RemoteState -> ( LocalState, Cmd Event )
-init _ = 
-    cmdNone Dict.empty
+                    else
+                        ( dealId
+                        , Deal id productId price qty |> Just
+                        , DealOpened id productId price qty
+                        )
 
-
-update : RemoteState -> Event -> LocalState -> ( LocalState, Cmd Event )
-update remote event local =
-    case event of
-        DealOpened dealID productID price quantity ->
-            ( ( local |> Dict.insert dealID (Deal dealID productID price quantity) ), Cmd.none)
-
-        DealClosed dealID ->
-            cmdNone ( local |> Dict.remove dealID )
-
-
-subscriptions remote local =
-    Sub.none
-
-
--- Extra types used in the API and state
-
-
-type OpenRequestFault
-    = InvalidQuantity ID Int
-    | InvalidPrice ID Price
-    | DuplicateDeal ID
-
-
-type CloseRequestFault
-    = DealNotFound ID
+                CloseDeal _ ->
+                    ( dealId, deal, DealNotFound dealId )
