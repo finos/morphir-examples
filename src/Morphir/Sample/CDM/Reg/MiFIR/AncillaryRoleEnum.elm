e5a15b4a41d4a7aada0c92d8fe285ce9d50744c6
Stephen Goldbaum <Stephen.Goldbaum@morganstanley.com>
2020-12-30 15:41:55 -0500

    Fuller CDM data structures

---
+++ src/Morphir/Sample/CDM/Reg/MiFIR/AncillaryRoleEnum.elm
@@ -0,0 +1,23 @@
+module Morphir.Sample.CDM.Reg.MiFIR.AncillaryRoleEnum exposing (AncillaryRoleEnum(..))
+
+
+type AncillaryRoleEnum
+    = DisruptionEventsDeterminingParty
+    | ExtraordinaryDividendsParty
+    | PredeterminedClearingOrganizationParty
+    | ExerciseNoticeReceiverPartyManual
+    | ExerciseNoticeReceiverPartyOptionalEarlyTermination
+    | ExerciseNoticeReceiverPartyCancelableProvision
+    | ExerciseNoticeReceiverPartyExtendibleProvision
+    | CalculationAgentIndependent
+    | CalculationAgentOptionalEarlyTermination
+    | CalculationAgentMandatoryEarlyTermination
+    | CalculationAgentFallback
+    | ArrangingBroker
+    | Beneficiary
+    | ClearingFirm
+    | CounterpartyAfflilate
+    | Guarantor
+    | OtherParty
+    | PrimeBroker
+    | SettlementAgent
--- src/Morphir/Sample/CDM/Reg/MiFIR/RTS22.elm
+++ src/Morphir/Sample/CDM/Reg/MiFIR/RTS22.elm
@@ -1,4 +1,69 @@
-module Morphir.Sample.CDM.MiFIR.RTS22 exposing (..)
+module Morphir.Sample.CDM.Reg.MiFIR.RTS22 exposing (..)
+
+import List.Nonempty as Nonempty exposing (Nonempty)
+import Morphir.SDK.LocalDate exposing (LocalDate)
+import Morphir.Sample.CDM.Reg.MiFIR.AncillaryRoleEnum exposing (AncillaryRoleEnum)
+import Morphir.Sample.CDM.Reg.MiFIR.FloatingRateIndexEnum exposing (FloatingRateIndexEnum(..))
+
+
+type alias Report =
+    { price : Price
+
+    --, reportStatus : ReportStatus
+    --, transactionReferenceNumber : TransactionReferenceNumber
+    --, tradingVenueTransactionIdentificationCode : TradingVenueTransactionIdentificationCode
+    --, executingEntityIdentificationCode : ExecutingEntityIdentificationCode
+    --, isInvestmentFirm : IsInvestmentFirm
+    --, submittingEntityIdentificationCode : SubmittingEntityIdentificationCode
+    --, buyerSeller : BuyerSeller
+    --, transmissionOfOrderIndicator : TransmissionOfOrderIndicator
+    --, tradingDateTime : TradingDateTime
+    --, tradingCapacity : TradingCapacity
+    , quantity : Quantity
+
+    --, venueOfExecution : VenueOfExecution
+    --, countryOfTheBranchMembership : CountryOfTheBranchMembership
+    --, instrumentIdentificationCode : InstrumentIdentificationCode
+    --, instrumentFullName : InstrumentFullName
+    --, instrumentClassification : InstrumentClassification
+    --, notionalCurrency1 : NotionalCurrency1
+    --, notionalCurrency2 : NotionalCurrency2
+    --, priceMultiplier : PriceMultiplier
+    --, underlyingInstrumentCode : UnderlyingInstrumentCode
+    --, underlyingIndexName : UnderlyingIndexName
+    --, underlyingIndexTermPeriod : UnderlyingIndexTermPeriod
+    --, underlyingIndexTermMultiplier : UnderlyingIndexTermMultiplier
+    --, expiryDate : ExpiryDate
+    --, deliveryType : DeliveryType
+    --, investmentDecisionWithinFirm : InvestmentDecisionWithinFirm
+    --, personResponsibleForInvestmentDecisionCountry : PersonResponsibleForInvestmentDecisionCountry
+    --, executionWithinFirm : ExecutionWithinFirm
+    --, personResponsibleForExecutionCountry : PersonResponsibleForExecutionCountry
+    --, commodityDerivativeIndicator : CommodityDerivativeIndicator
+    --, securitiesFinancingTransactionIndicator : SecuritiesFinancingTransactionIndicator
+    }
+
+
+type alias Quantity =
+    { amount : Amount
+    , unit : Unit
+    }
+
+
+type alias Number =
+    Float
+
+
+type alias Amount =
+    Float
+
+
+type Unit
+    = MWH
+    | MMBTU
+    | BBL
+    | GAL
+    | BSH
 
 
 type PriceType
@@ -6,11 +71,6 @@ type PriceType
     | FixedFloatPrice
     | BasisSwapPrice
     | CDSPrice
-
-
-type alias Report =
-    { price : Float
-    }
 
 
 type alias PriceNotation =
@@ -28,18 +88,17 @@ type alias Price =
     }
 
 
-type alias FloatingInterestRate =
-    { initialRate : Float
-    , initialRate : Maybe Float
-    , spread : Maybe Float
-    , capRate : Maybe Float
-    , floorRate : Maybe Float
-    , multiplier : Maybe Float
+type alias FixedInterestRate =
+    { rate : Float
     }
 
 
-type alias FixedInterestRate =
-    { rate : Float
+type alias FloatingInterestRate =
+    { initialRate : Maybe Number
+    , spread : Maybe Number
+    , capRate : Maybe Number
+    , floorRate : Maybe Number
+    , multiplier : Maybe Number
     }
 
 
@@ -48,10 +107,139 @@ type alias Trade =
     }
 
 
+type alias Identifier =
+    { issuerReference : Maybe Party
+    , issuer : Maybe String
+    , assignedIdentifier : AssignedIdentifier
+    }
+
+
+type alias AssignedIdentifier =
+    { identifier : String
+    , version : Maybe Version
+    }
+
+
+type alias Party =
+    { partyId : String
+    , name : Maybe String
+    , person : List NaturalPerson
+
+    --, account : Maybe Account
+    }
+
+
+type alias Account =
+    { partyReference : Maybe Party
+    , accountNumber : String
+    , accountName : Maybe String
+    , accountType : Maybe AccountTypeEnum
+    , accountBeneficiary : Maybe Party
+    , servicingParty : Maybe Party
+    }
+
+
+type AccountTypeEnum
+    = AccountTypeEnum
+    | AggregateClient
+    | Client
+    | House
+
+
+type alias NaturalPerson =
+    { honorific : Maybe String
+    , firstName : String
+    , middleName : List String
+    , initial : List String
+    , surname : String
+    , suffix : Maybe String
+    , dateOfBirth : Maybe LocalDate
+    }
+
+
+type alias Version =
+    Int
+
+
 type alias TradableProduct =
     { product : Product
-    , priceNotation : List PriceNotation
+    , quantityNotation : Nonempty QuantityNotation
+    , priceNotation : Nonempty PriceNotation
+    , counterparty : ( Counterparty, Counterparty )
+    , ancillaryParty : List AncillaryParty
+    , adjustment : Maybe NotionalAdjustmentEnum
     }
+
+
+type NotionalAdjustmentEnum
+    = Execution
+    | PortfolioRebalancing
+    | Standard
+
+
+type alias Counterparty =
+    { role : CounterpartyRoleEnum
+    , partyReference : Party
+    }
+
+
+type alias AncillaryParty =
+    { role : AncillaryRoleEnum
+    , partyReference : Nonempty Party
+    , onBehalfOf : Maybe CounterpartyRoleEnum
+    }
+
+
+type CounterpartyRoleEnum
+    = Party1
+    | Party2
+
+
+type alias QuantityNotation =
+    { quantity : NonNegativeQuantity
+    , assetIdentifier : AssetIdentifier
+    }
+
+
+type alias NonNegativeQuantity =
+    -- TODO
+    Float
+
+
+type alias AssetIdentifier =
+    { productIdentifier : Maybe ProductIdentifier
+    , currency : Maybe Currency
+    , rateOption : Maybe FloatingRateOption
+    }
+
+
+type alias FloatingRateOption =
+    { floatingRateIndex : FloatingRateIndexEnum
+    , indexTenor : Maybe Period
+    }
+
+
+type alias Period =
+    { periodMultiplier : Int
+    , period : PeriodEnum
+    }
+
+
+type PeriodEnum
+    = D
+    | W
+    | M
+    | Y
+
+
+type alias ProductIdentifier =
+    { identifier : String
+    }
+
+
+type alias Currency =
+    -- TODO
+    String
 
 
 type alias Product =
@@ -155,6 +343,7 @@ extract FixedInterestRate -> rate as "Price"
 fixedFixedPrice : Trade -> Maybe Float
 fixedFixedPrice trade =
     trade.tradableProduct.priceNotation
+        |> Nonempty.toList
         |> List.filterMap (\x -> x.price.fixedInterestRate)
         |> List.map .rate
         |> List.maximum
@@ -186,6 +375,7 @@ fixedFloatPrice : Trade -> Maybe Float
 fixedFloatPrice trade =
     -- TODO : Is this the correct interpretation of the above?
     trade.tradableProduct.priceNotation
+        |> Nonempty.toList
         |> List.filterMap (\x -> x.price.fixedInterestRate)
         |> List.map .rate
         |> List.maximum
@@ -210,6 +400,7 @@ isIRSwapBasis trade =
 basisSwapPrice : Trade -> Maybe Float
 basisSwapPrice trade =
     trade.tradableProduct.priceNotation
+        |> Nonempty.toList
         |> List.filterMap (\x -> x.price.floatingInterestRate)
         |> List.filterMap .initialRate
         |> List.head
@@ -229,6 +420,7 @@ cdsPrice trade =
     let
         mp =
             trade.tradableProduct.priceNotation
+                |> Nonempty.toList
                 |> List.head
                 |> Maybe.map .price
 
